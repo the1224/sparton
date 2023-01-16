03752dfefe66935fa5ff0a053961546eaf696751
dkssudtp1 <dkssudtp1@naver.com>
2023-01-16 16:09:27 +0900

    feat : 투표 하기

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from vote import *
 
 app = Flask(__name__)
@@ -11,9 +11,9 @@ def index():
 def vote_poll():
   return render_template('vote-poll.html')
 
-@app.route('/vote')
-def vote():
-  return render_template('vote.html')
+@app.route('/vote/<id>')
+def vote(id):
+  return render_template('vote.html', id = id)
 
 @app.route('/vote-result')
 def vote_result():
@@ -25,8 +25,9 @@ def shared_vote_links():
 
 # 투표 GET API
 @app.route('/api/vote', methods=["GET"])
-def vote_get_api():
-  return vote_get()
+def vote_get_data():
+  id = request.args.get('idGive', "0")
+  return vote_get(id)
 
 # 투표 POST API
 @app.route('/api/vote', methods=["POST"])
